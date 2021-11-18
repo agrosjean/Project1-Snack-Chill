aa5058fa8516c2b5fa72f056f57fc666129688a8
alexandre <agrosjean007@gmail.com>
2021-11-17 19:58:29 -0500

    JS is done

--- assets/index.js
+++ assets/index.js
@@ -7,6 +7,7 @@ const API_KEY = 'api_key=b38b724637a214decfd523fff0073f29';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY
 
+console.log(API_URL)
 
 let movieDisplay = document.getElementById('movie-display')
 
@@ -17,47 +18,64 @@ let rate = document.getElementById('vote-average')
 
 // Snack
 const snackURL = 'http://localhost:3000/snacks'
+let snackDisplay = document.getElementById('snack-display')
+let snackImg = document.getElementsByTagName('img')[1]
+let nameS = document.getElementById('name')
+
+// Comment
+let comment = document.getElementById('comment')
 
 getMovie(API_URL);
 
 async function getMovie(url) {
+    let clickedGenerate = false;
     const snacks = await getSnacks(snackURL)
 
     fetch(url)
         .then(response => response.json())
         .then(movieArray => {
+
             bttn.addEventListener('click', () => {
-                GeneratorMS(movieArray.results);
+
+                if (clickedGenerate == false) {
+                    //Create the form...
+                    let form = document.createElement('form')
+                    let input = document.createElement('input')
+                    let submit = document.createElement('input')
+                    submit.type = 'submit'
+                    form.append(input, submit)
+                    comment.append(form)
+
+                    form.addEventListener('submit', (e) => {
+                        e.preventDefault();
+                        input.value = ""
+                    })
+
+                    clickedGenerate = true
+                }
+                GeneratorMS(movieArray.results, snacks);
 
             })
-
-
         })
 }
-
-// Function render Movie
-// Function render Snacks
 
 function getSnacks(snackURL) {
     return fetch(snackURL)
         .then(response => response.json())
-        .then(snackArray => {
-            console.log(snackArray)
-            // return snackArray.snacks
 
-        })
 }
-
 
 function GeneratorMS(movieArray, snacks) {
 
     // get random index value
     const randomIndex = Math.floor(Math.random() * movieArray.length)
-    // const randomIndexS = Math.floor(Math.random() * snacks.length)
+
+    const randomIndexS = Math.floor(Math.random() * snacks.length)
+
     // get random item
     const item = movieArray[randomIndex];
-    // const snackItem = snacks[randomIndexS]
-    // console.log(snackItem)
+    const snackItem = snacks[randomIndexS]
+
     // // Movie
     // Image display
     movieImg.src = 'https://image.tmdb.org/t/p/w500' + item.backdrop_path
@@ -67,21 +85,20 @@ function GeneratorMS(movieArray, snacks) {
     overview.innerHTML = item.overview
     // Rate
     rate.innerHTML = item.vote_average
-
     // Snack
+    // Image
+    snackImg.src = snackItem.image
 
+    // Name 
+    nameS.innerHTML = snackItem.name
     return item
-
-
 
 }
 
-// API movie - done
 
 
-// API snacks - db.json - make at least 10/15
 
-// Figure math.random for movie and snacks
 
-// addEventListener for Generator 'click'
+
+
 
